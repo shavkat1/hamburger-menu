77b590048c7b3344b955fa05dc63cccd5d0e7139
shavkat1 <81327341+shavkat1@users.noreply.github.com>
2021-05-03 01:14:17 +0500

    hamburger-menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('.nav-lines')
+const navbar = document.querySelector('.nav')
+
+
+menuIcon.addEventListener("click", () =>{
+    navbar.classList.toggle("change")
+})
